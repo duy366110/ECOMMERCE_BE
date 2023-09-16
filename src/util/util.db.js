e98a3b08c7fd48d90f677378c18823c3f1b9bde0
duy366110 <duy366110@gmail.com>
2023-09-16 16:31:14 +0700

    #Commit 16/09/2023: Config BE connect DB cloud.

--- src/util/util.db.js
+++ src/util/util.db.js
@@ -5,7 +5,7 @@ class mongodb {
     constructor() { }
 
     connect = (callback) => {
-        mongoose.connect('mongodb://127.0.0.1:27017/ass_03')
+        mongoose.connect('mongodb+srv://duy366110:jvLAaEtOIsc8EBuM@normal.29rxu4p.mongodb.net/ecommerce?retryWrites=true&w=majority') // mongodb://127.0.0.1:27017/ass_03
         .then(() => {
             callback();
         })
