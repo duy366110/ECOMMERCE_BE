b3a56006268c50cff76121fe014a1e7eca0e75bb
duy366110 <duy366110@gmail.com>
2023-09-16 15:23:48 +0700

    #Commit 16/09/2023: Config cors ecommerce be application.

--- src/environment.js
+++ src/environment.js
@@ -2,7 +2,8 @@ const Environment = {
     cors: {
         origins: [
             'http://localhost:3001',
-            'http://localhost:3000'
+            'http://localhost:3000',
+            'https://ecommercead-cfaf5.web.app'
         ]
     },
     port: 5000,
