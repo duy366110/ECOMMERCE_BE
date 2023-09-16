d9397489613e5a1fae0fcf103e7d68bf72f847c8
duy366110 <duy366110@gmail.com>
2023-09-16 22:40:26 +0700

    #Commit 16/09/2023: Config cors https form FE client.

--- src/environment.js
+++ src/environment.js
@@ -4,7 +4,9 @@ const Environment = {
             'http://localhost:3001',
             'http://localhost:3000',
             'https://ecommercead-cfaf5.web.app',
-            'https://ecommercead-cfaf5.firebaseapp.com'
+            'https://ecommercead-cfaf5.firebaseapp.com',
+            'https://ecommerce-6d965.web.app',
+            'https://ecommerce-6d965.firebaseapp.com'
         ]
     },
     port: 5000,
